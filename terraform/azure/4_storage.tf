b56df021d3fadc0581ca319b52c6ec91bfeea3ce
Tho Nguyen <tho.nguyen@savoirfairelinux.com>
2017-01-30 17:04:39 -0500

    Fix dependency on resource group

--- terraform/azure/4_storage.tf
+++ terraform/azure/4_storage.tf
@@ -3,8 +3,9 @@
 /////////////////////////
 
 resource "azurerm_storage_account" "bosh-azure-storage-account" {
-    name = "boshstore"
-    resource_group_name = "${var.env_name}"
-    location = "${var.location}"
-    account_type = "Standard_LRS"
+  name = "boshstore"
+  depends_on          = ["azurerm_resource_group.bosh_resource_group"]
+  resource_group_name = "${var.env_name}"
+  location = "${var.location}"
+  account_type = "Standard_LRS"
 }
