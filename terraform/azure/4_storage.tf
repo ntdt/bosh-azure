ca6f48129ba3c644c3d5756d2bfe3fd5c2c4b9be
Tho Nguyen <tho.nguyen@savoirfairelinux.com>
2017-01-30 17:03:06 -0500

    Change storage account name

--- terraform/azure/4_storage.tf
+++ terraform/azure/4_storage.tf
@@ -3,7 +3,7 @@
 /////////////////////////
 
 resource "azurerm_storage_account" "bosh-azure-storage-account" {
-    name = "bosh-store"
+    name = "boshstore"
     resource_group_name = "${var.env_name}"
     location = "${var.location}"
     account_type = "Standard_LRS"
