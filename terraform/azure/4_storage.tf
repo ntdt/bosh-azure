6ee441d9db4d8132cfe2721156c309ddc570bddd
Tho Nguyen <tho.nguyen@savoirfairelinux.com>
2017-01-31 10:27:59 -0500

    Add missing storage container and table

--- terraform/azure/4_storage.tf
+++ terraform/azure/4_storage.tf
@@ -3,9 +3,30 @@
 /////////////////////////
 
 resource "azurerm_storage_account" "bosh-azure-storage-account" {
-  name = "myboshstorage"
+  name                = "myboshstorage"
   depends_on          = ["azurerm_resource_group.bosh_resource_group"]
   resource_group_name = "${var.env_name}"
-  location = "${var.location}"
-  account_type = "Standard_LRS"
+  location            = "${var.location}"
+  account_type        = "Standard_LRS"
+}
+
+resource "azurerm_storage_container" "bosh" {
+  name                  = "bosh"
+  resource_group_name   = "${var.env_name}"
+  storage_account_name  = "${azurerm_storage_account.bosh-azure-storage-account.name}"
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "stemcell" {
+  name                  = "stemcell"
+  resource_group_name   = "${var.env_name}"
+  storage_account_name  = "${azurerm_storage_account.bosh-azure-storage-account.name}"
+  container_access_type = "blob"
+}
+
+resource "azurerm_storage_table" "stemcells" {
+  name                 = "stemcells"
+  resource_group_name  = "${var.env_name}"
+  storage_account_name = "${azurerm_storage_account.bosh-azure-storage-account.name}"
+  
 }
